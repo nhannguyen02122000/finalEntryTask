af7900e8936135a3fcbec69dd11aa6c3b809d606
NhanNguyenThanh <nhannguyenthanh@ShopeeVNs-MacBook-Pro.local>
2021-07-09 14:02:59 +0700

    CSR

---
+++ pages/csr/index.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import useSWR from 'swr'
+import Loader from "react-loader-spinner";
+import loaderStyles from '../../styles/loader.module.css'
+import SpeakerCard from '../../components/speakerCard';
+import styles from '../../styles/carddeck.module.css'
+import React, { useEffect, useState } from 'react'
+
+const CSR = () => {
+    const [isComponentMounted, setIsComponentMounted] = useState(false);
+    const {data, error} = useSWR('https://randomuser.me/api/?results=1000');
+    useEffect(() => setIsComponentMounted(true), []);
+
+    if(!isComponentMounted) {
+        return <div className={loaderStyles.central}>
+        <Loader
+          type="MutatingDots"
+          color="#6ac5fe"
+          secondaryColor = "Grey"
+          height={100}
+          width={100}
+        />
+        </div>
+    }
+  
+    if (!data && !error) {
+      return <div className={loaderStyles.central}>
+      <Loader
+        type="MutatingDots"
+        color="#6ac5fe"
+        secondaryColor = "Grey"
+        height={100}
+        width={100}
+      />
+      </div>
+    }
+    if (error) {
+      return <h1>Data cannot be fetched!</h1>
+    }
+  
+    return (
+      <>
+      <Head>
+        <title>CSR</title>
+      </Head>
+      <div className="container">
+        <div className="row">
+          <h1>CSR: HTML + fetch data</h1>
+          <div className={`card-deck ${styles.cardContainer}`}>
+              {data.results.map((ele, idx) =>
+                <SpeakerCard 
+                  key = {idx}
+                  idx = {idx}
+                  title = {ele.name.title}
+                  src = {ele.picture.large}
+                  firstName = {ele.name.first}
+                  lastName = {ele.name.last}
+                  userName = {ele.email}
+                  ele = {ele}
+                />
+              )}
+          </div>
+        </div>
+      </div>
+      </>
+    )
+  }
+  
+  export default CSR;
