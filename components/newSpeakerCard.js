712deaccb4adcd81c026b874d1652e7c3916c14c
NhanNguyenThanh <nhannguyenthanh@ShopeeVNs-MacBook-Pro.local>
2021-07-09 16:47:18 +0700

    Add react windows

---
+++ components/newSpeakerCard.js
@@ -0,0 +1,31 @@
+import styles from '../styles/carddeck.module.css'
+
+const NewSpeakerCard = ({ele, style, GUTTER_SIZE}) => {
+  return(
+    <div 
+      style={{
+      ...style,
+      left: style.left + GUTTER_SIZE,
+      top: style.top + GUTTER_SIZE,
+      width: style.width - GUTTER_SIZE,
+      height: style.height - GUTTER_SIZE
+      }}
+      className={`card`}>
+      <div className={`${styles.cardImg}`}>
+      </div>
+      <div className="card-body">
+        <h5 className="card-title">{`${ele.name.first} ${ele.name.last}`}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{ele.name.title}</h6>
+        <p className="card-text">{ele.login.username}</p>
+        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">{`Age: ${ele.dob.age}`}</li>
+        <li className="list-group-item">{`Country: ${ele.location.country}`}</li>
+        <li className="list-group-item">{`Phone: ${ele.phone}`}</li>
+      </ul>
+    </div>
+  )
+}
+
+export default NewSpeakerCard;
---
+++ pages/ssg/withrw.js
@@ -0,0 +1,69 @@
+import Head from 'next/head'
+import SpeakerCard from '../../components/speakerCard';
+import NewSpeakerCard from '../../components/newSpeakerCard';
+import styles from '../../styles/carddeck.module.css'
+import Loader from "react-loader-spinner";
+import AutoSizer from 'react-virtualized-auto-sizer'
+import {FixedSizeGrid as Grid} from 'react-window'
+
+const GUTTER_SIZE = 10;
+
+export async function getStaticProps () {
+  const respond = await fetch('https://randomuser.me/api/?results=1000');
+  const data = await respond.json();
+
+  return {
+    props: {data: data.results}
+  }
+}
+
+const Cell = ({ele, style}) => (
+  <div 
+    style={{
+      ...style,
+      left: style.left + GUTTER_SIZE,
+      top: style.top + GUTTER_SIZE,
+      width: style.width - GUTTER_SIZE,
+      height: style.height - GUTTER_SIZE
+    }}
+    className={`card`}>
+    <div className={`${styles.cardImg}`}>
+      <Image src={ele.picture.large} width={200} height={200} alt={`user_${ele.name.first}`}/>
+    </div>
+  </div>
+);
+
+const SSGRW = (props) => {
+  const data = props.data;
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <h1>SSG: HTML + fetch data</h1>
+          <Grid
+            columnCount={3}
+            columnWidth={300+GUTTER_SIZE}
+            rowHeight={300+GUTTER_SIZE}
+            height={560}
+            width={1000}
+            rowCount={1000}
+          >
+            {({columnIndex, rowIndex, style}) => {
+              return (
+                <NewSpeakerCard
+                  ele = {data[columnIndex*3+rowIndex]}
+                  style = {style}
+                  GUTTER_SIZE = {GUTTER_SIZE}
+                />
+              )
+            }}
+          </Grid>
+        </div>
+      </div>
+    </>
+    
+  )
+}
+
+export default SSGRW;
