804b6163ac640ec3a8cb7f072d0adf2dcbbc2b3c
NhanNguyenThanh <nhannguyenthanh@ShopeeVNs-MacBook-Pro.local>
2021-07-09 14:18:12 +0700

    Fix menu

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import styles from '../styles/menubutton.module.css'
 
 export default function Home() {
   return (
@@ -7,18 +8,23 @@ export default function Home() {
     <Head>
       <title>Home page</title>
     </Head>
-    <Link href="/ssg" passHref>
-      <button type="button" className="btn btn-primary">SSG</button>
-    </Link>
-    <Link href="/csr" passHref>
-      <button type="button" className="btn btn-primary">CSR</button>
-    </Link>
-    <Link href="/ssg-csr" passHref>
-      <button type="button" className="btn btn-primary">SSG+CSR</button>
-    </Link>
-    <Link href="/ssr-csr" passHref>
-      <button type="button" className="btn btn-primary">SSR+CSR</button>
-    </Link>
+    <div className={styles.title}>
+      <h1>Choose mode</h1>
+    </div>
+    <div className={styles.menu}>
+      <Link href="/ssg" passHref>
+        <button type="button" className="btn btn-primary">SSG</button>
+      </Link>
+      <Link href="/csr" passHref>
+        <button type="button" className="btn btn-primary">CSR</button>
+      </Link>
+      <Link href="/ssg-csr" passHref>
+        <button type="button" className="btn btn-primary">SSG+CSR</button>
+      </Link>
+      <Link href="/ssr-csr" passHref>
+        <button type="button" className="btn btn-primary">SSR+CSR</button>
+      </Link>
+    </div>
     </>
   )
 }
